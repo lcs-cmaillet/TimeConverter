a2559e630e5c395e51fa4a9448b5b37333cee212
Collin Maillet <cmaillet@lcs.on.ca>
2025-04-04 11:43:51 -0400

    added navigation title.

--- TimeConverter/Views/HourConversionView.swift
+++ TimeConverter/Views/HourConversionView.swift
@@ -15,37 +15,39 @@ struct HourConversionView: View {
     //MARK: Computed properties
     var body: some View {
         
-        VStack {
-            
-            //OUTPUT
-            
-            // We will unwrap the optional convertionResult if possible and show result
-            if let conversionResult = viewModel.conversionResult{
+        NavigationStack {
+            VStack {
                 
-                VStack {
-                    Text("\(conversionResult.timeInHours)")
-                   
-                    Text("\(conversionResult.timeInMinutes)")
-                   
-                    Text("\(conversionResult.timeInSeconds)")
-                }
-                .frame(height:200)
+                //OUTPUT
                 
-                
-               
-            } else {
-                
-                ContentUnavailableView("Unable to preform convertion", systemImage: "gear.badge.questionmark", description: Text(viewModel.recoverySuggestion))
+                // We will unwrap the optional convertionResult if possible and show result
+                if let conversionResult = viewModel.conversionResult{
+                    
+                    VStack {
+                        Text("\(conversionResult.timeInHours)")
+                       
+                        Text("\(conversionResult.timeInMinutes)")
+                       
+                        Text("\(conversionResult.timeInSeconds)")
+                    }
                     .frame(height:200)
+                    
+                    
+                   
+                } else {
+                    
+                    ContentUnavailableView("Unable to preform convertion", systemImage: "gear.badge.questionmark", description: Text(viewModel.recoverySuggestion))
+                        .frame(height:200)
+                    
+                }
+                
+                //IMPUT
+                 TextField("Enter the number of hours", text: $viewModel.providedHours)
+                     .textFieldStyle(.roundedBorder)
                 
             }
-            
-            //IMPUT
-             TextField("Enter the number of hours", text: $viewModel.providedHours)
-                 .textFieldStyle(.roundedBorder)
-            
+            .navigationTitle("Time Convertion")
         }
-            
 
         
         
