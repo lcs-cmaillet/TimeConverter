4cc4aa2b08adcad62177f73c2c5b0ccf45b390f4
Collin Maillet <cmaillet@lcs.on.ca>
2025-04-04 11:15:47 -0400

    Initial Commit

---
+++ TimeConverter/Views/HourConversionView.swift
@@ -0,0 +1,24 @@
+//
+//  HourConversionView.swift
+//  TimeConverter
+//
+//  Created by Russell Gordon on 2025-02-24.
+//
+
+import SwiftUI
+
+struct HourConversionView: View {
+    
+    //MARK: Stored Properties
+    
+    
+    //MARK: Computed properties
+    var body: some View {
+        Text("Will show convertions soon")
+    }
+}
+
+
+#Preview {
+    HourConversionView()
+}
